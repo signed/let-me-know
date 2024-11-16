37db2d22be26b6b2f1b34d1d3363cc65fa41ea3f
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-11-16 20:09:47 +0100

    basic setup

---
+++ src/release-check.tests.ts
@@ -0,0 +1,33 @@
+import { expect, test } from 'vitest'
+import { type CheerioAPI, load } from 'cheerio'
+
+let page: string
+
+test('taetigkeitsschluessel', async () => {
+  page = 'https://www.arbeitsagentur.de/unternehmen/betriebsnummern-service/taetigkeitsschluessel'
+  expect(
+    await latestVersionAt(($) => $('h3:contains("Download aktuelle Version")').parent().children('p + p').text()),
+  ).toEqual('Schlüsselverzeichnis 2010 – Stand 03/2024 (2,3 MB)')
+})
+
+const latestVersionAt = async (extractor: ($: CheerioAPI) => string): Promise<string> => {
+  let api = await fetchAndParsePage(page)
+  if (api === 'failed') {
+    return 'failed to load and parse '
+  }
+  try {
+    return extractor(api)
+  } catch (e) {
+    return 'failed to extract version information'
+  }
+}
+
+async function fetchAndParsePage(page: string): Promise<CheerioAPI | 'failed'> {
+  try {
+    const response = await fetch(page)
+    const html = await response.text()
+    return load(html)
+  } catch (e) {
+    return 'failed'
+  }
+}
